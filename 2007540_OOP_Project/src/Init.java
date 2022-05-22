f9ebd70a27e7c305f944d9cafdbd7b86477a646d
Jack <jack.sargeant22@gmail.com>
2022-05-22 22:04:38 +0100

    sqgqdgqdgdqg

--- 2007540_OOP_Project/src/Init.java
+++ 2007540_OOP_Project/src/Init.java
@@ -1,14 +1,35 @@
 import com.google.gson.*;
+import com.google.gson.stream.JsonReader;
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 public class Init {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
 //https://stackoverflow.com/questions/42299871/how-generic-method-in-java-interface-convert-super-object-to-concrete-subclass-o
 
+        Gson gson = new Gson();
+
+        FileReader reader = new FileReader("C:\\2007540_course_ms\\2007540_OOP_Project\\data\\staff.json");
+        JsonReader jsonReader = new JsonReader(reader);
+
+        Student[] users = gson.fromJson(jsonReader, Student[].class);
+
+        List<User> userList = new ArrayList<User>(Arrays.asList(users));
+
+
+        for (User user : userList
+             ) {
+            System.out.println(user.getEmail());
+        }
     }
 
 }
