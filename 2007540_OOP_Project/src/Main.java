493cb6236b4ef1c773cc6a4ee221fb5c6b643c38
Jack <jack.sargeant22@gmail.com>
2022-06-15 12:14:49 +0100

    dgqgqdgqdgdsa

--- 2007540_OOP_Project/src/Main.java
+++ 2007540_OOP_Project/src/Main.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.ArrayList;
 
 
@@ -9,17 +10,23 @@ public class Main implements Dependencies{
 
         //CHANGE JSON PARSER TO BE JSONABLE VIA INTERFACE
         //ADD MORE POLYMORPHIC & ABSTRACT ELEMENTS
+        //ADD ACTION LISTEN
         //ERROR CHECKING
         //make naming less ambiguous
+        // ADD A NEW STUDENT OR STAFF
         //FINAL vars
 
 
         ArrayList<Departments> departmentStructres = Initialise.InitialiseDepartments();
 
         assert departmentStructres != null;
-        final GUI gui = new GUI(departmentStructres);
+//        final GUI gui = new GUI(departmentStructres);
 
-
+        File structureFile = FileParser.createFile("test","2007540_OOP_Project/data/");
+        assert structureFile != null;
+        FileParser.writeToFile(structureFile,"1");
+        FileParser.writeToFile(structureFile,"2");
+        FileParser.writeToFile(structureFile,"3");
 
     }
 
